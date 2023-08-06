e9070625be330215cd332992fc35f0e857947af8
ducnguyen96 <levinguyen.dl@gmail.com>
2023-08-06 10:01:51 +0700

    change default keys

--- consts.lua
+++ consts.lua
@@ -3,8 +3,8 @@
 -- If you do not like this or do not have such a key,
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod1"
-altkey = "Mod4"
+modkey = "Mod4"
+altkey = "Mod1"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
